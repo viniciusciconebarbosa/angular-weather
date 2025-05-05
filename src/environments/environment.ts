61d762608b2c5b6e124809e8f528328d3df19d81
vinicius <viniciuscicone@gmail.com>
2025-05-04 21:04:52 -0300

    update dotenv angular config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
-declare const process: any; // Declaração para evitar erro do TypeScript
+declare const process: any; // Necessário para TypeScript
 
 export const environment = {
-  production: true,
+  production: false,
   geocodingApiKey: process.env['ANGULAR_GEOCODING_API_KEY'],
   weatherApiKey: process.env['ANGULAR_WEATHER_API_KEY'],
   geocodingApiUrl: process.env['ANGULAR_GEOCODING_API_URL'] || 'https://api.api-ninjas.com/v1/geocoding',
   weatherApiUrl: process.env['ANGULAR_WEATHER_API_URL'] || 'https://weather.googleapis.com/v1/forecast/days:lookup',
   defaultDays: Number(process.env['ANGULAR_DEFAULT_DAYS']) || 10
-};
+}; 
